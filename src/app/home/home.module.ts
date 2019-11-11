da9ba8a3d7b712cff8c6418d8abd197aa34b1a1f
daliyajohn <daliyajohn75@gmail.com>
2019-11-11 16:02:03 +0530

    feat(form): implement reactive form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { AddUserComponent } from './add-user/add-user.component';
 import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  declarations: [HomeComponent, UserListComponent, AddUserComponent, UserFormComponent],
+  declarations: [HomeComponent, UserListComponent, UserFormComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
--- src/app/home/user-form/user-form.component.ts
+++ src/app/home/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
-
-  constructor() { }
+  userForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required]
+    });
+  }
+  // submit data
+  onSubmit(userData) {
+    this.submitted = true;
+    console.log('>>>>>>>>>>>>>>>', userData);
+    if (this.userForm.invalid) {
+        return;
+    }
   }
 
+  onReset() {
+    this.submitted = false;
+    this.userForm.reset();
+  }
 }
