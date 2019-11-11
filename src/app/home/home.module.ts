0e44ecf9599b63bed563c7dda970e98c98d033fe
daliyajohn <daliyajohn75@gmail.com>
2019-11-11 15:43:23 +0530

    feat(home): add user form ui

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  declarations: [HomeComponent, UserListComponent, AddUserComponent],
+  declarations: [HomeComponent, UserListComponent, AddUserComponent, UserFormComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
