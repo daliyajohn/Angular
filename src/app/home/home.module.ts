33e7f346f9218b98182b0b5c6c739fc4abc0b427
daliyajohn <daliyajohn75@gmail.com>
2019-11-11 14:18:02 +0530

    feat(home): create add user component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
-  declarations: [HomeComponent, UserListComponent],
+  declarations: [HomeComponent, UserListComponent, AddUserComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
