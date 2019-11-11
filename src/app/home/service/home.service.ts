bd4a91688815f9a14574fe23569d5a9b797ee8c3
daliyajohn <daliyajohn75@gmail.com>
2019-11-11 17:48:28 +0530

    feat(user-list): modify edit update ui

--- src/app/home/service/home.service.ts
+++ src/app/home/service/home.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  private customerApiUrl = 'customers';
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'https://reqres.in/api/users?page=1';
 
-  constructor() { }
+  getUsersData() {
+    return this.http.get(this.baseUrl);
+  }
+
+
 }
--- src/app/home/user-list/user-list.component.ts
+++ src/app/home/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeService } from '../service/home.service';
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+  usersData: any;
+  constructor( private router: Router, private homeService: HomeService) { }
 
   ngOnInit() {
+    // user data list
+    this.homeService.getUsersData().subscribe( data => {
+      this.usersData = data;
+      console.log('>>>>>>>><<<<<<<<<<<<', this.usersData);
+    });
   }
-
 }
