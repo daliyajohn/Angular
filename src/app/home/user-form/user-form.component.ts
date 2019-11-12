7674275777d1662d7cbf61c208fa77e40d400141
daliyajohn <daliyajohn75@gmail.com>
2019-11-12 17:38:16 +0530

    feat(form): update form field

--- src/app/home/user-form/user-form.component.ts
+++ src/app/home/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HomeService } from '../service/home.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-form',
@@ -7,23 +9,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  @Input() userEditData;
   userForm: FormGroup;
   submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(private formBuilder: FormBuilder, private homeService: HomeService) { 
+  }
 
   ngOnInit() {
+    console.log('jdsbsjcfjscfbks;jdbcfn', this.userEditData);
+    
+
     this.userForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      first_name: ['', Validators.required],
-      last_name: ['', Validators.required]
+      employee_name: ['', Validators.required],
+      employee_salary: ['', Validators.required],
+      employee_age: ['', Validators.required]
     });
   }
   // submit data
   onSubmit(userData) {
     this.submitted = true;
-    console.log('>>>>>>>>>>>>>>>', userData);
-    if (this.userForm.invalid) {
-        return;
+    // if (this.userForm.invalid) {
+    //    this.homeService.createUser(userData)
+    // }
+console.log(this.userForm)
+
+// const values = {
+//   'data': this.userForm.value,
+// };
+
+console.log('dlmsdkcfmsl;dcfsdl;cf', userData);
+    if (this.userForm.valid) {
+      this.homeService.createUser(userData).subscribe(data => {
+          if(data.status === 200) {
+            alert('User updated successfully.');
+          }else {
+            alert(data.message);
+          }
+        },
+        error => {
+          alert(error);
+        });
     }
   }
 
