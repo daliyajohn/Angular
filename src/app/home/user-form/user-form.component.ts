92c777d88350ea51ba0b4389982fb4def2a0f0b1
daliyajohn <daliyajohn75@gmail.com>
2019-12-31 11:57:24 +0530

    feat(form): remove seperate edit form

--- src/app/home/user-form/user-form.component.ts
+++ src/app/home/user-form/user-form.component.ts
@@ -13,22 +13,36 @@ export class UserFormComponent implements OnInit {
   userForm: FormGroup;
   submitted = false;
   @Output() cancelForm: EventEmitter<any> = new EventEmitter<any>();
+  popupTitle: any;
+  popupButton: any;
 
   constructor(private formBuilder: FormBuilder, private homeService: HomeService) { 
   }
 
   ngOnInit() { 
+    this.popupTitle = 'Add';
+    this.popupButton = 'Add';
     this.userForm = this.formBuilder.group({
       name: ['', Validators.required],
       salary: ['', Validators.required],
-      age: ['', Validators.required]
+      age: ['', Validators.required],
+      id: ['']
     });
+
+    if (this.userEditData && this.userEditData.id) {
+      this.popupTitle = 'Edit';
+      this.popupButton = 'Update';
+      this.userForm.get('name').setValue(this.userEditData.employee_name ? this.userEditData.employee_name : '');
+      this.userForm.get('salary').setValue(this.userEditData.employee_salary ? this.userEditData.employee_salary : '');
+      this.userForm.get('age').setValue(this.userEditData.employee_age ? this.userEditData.employee_age : '');
+      this.userForm.get('id').setValue(this.userEditData.id ? this.userEditData.id : '');
+    }
   }
 
   // submit data
   onSubmit(userData) {
     this.submitted = true;
-    if (this.userForm.valid) {
+    if (this.userForm.valid && !this.userEditData.id) {
       this.homeService.createUser(userData).subscribe(data => {
         this.cancelForm.emit(false);
         if(data.status === 200) {
@@ -41,6 +55,18 @@ export class UserFormComponent implements OnInit {
       error => {
         alert(error);
       });
+    }  
+    
+    if(this.userEditData && this.userEditData.id) {
+      const dataFormat = {
+        'name': userData.name,
+        'salary': userData.salary,
+        'age': userData.age,
+        'id': userData.id
+      }    
+      this.homeService.updateUserData(dataFormat).subscribe(r => {
+        this.cancelForm.emit(true);
+      })
     }
   }
 
